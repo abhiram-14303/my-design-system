13c8a127ef3562f50f9db63369868af7574b28b0
Abhiram <abhiram.dd@zohocorp.com>
2026-08-04 18:06:05 +0530

    fix: esm interop for rolldown

--- vite.config.lib.js
+++ vite.config.lib.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    conditions: ['import', 'module', 'browser', 'default'],
+  },
   build: {
     lib: {
       entry: 'src/index.js',
@@ -10,12 +13,10 @@ export default defineConfig({
       fileName: () => `bigin-design-system.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
+        format: 'es',
+        interop: 'esModule',
       },
     },
   },
