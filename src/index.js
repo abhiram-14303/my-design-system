97898f697f56701104e5469655e4eabfb8751092
Abhiram <abhiram.dd@zohocorp.com>
2026-08-04 16:09:59 +0530

    Setup npm package config for GitHub Packages

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// Bigin Design System — component exports
+// Screens (RightModal, SideMenu, PageSources, QuickPreview) and Icons are NOT exported —
+// they live in the dev site only.
+
+export { default as Button }       from './components/Button'
+export { default as Checkbox }     from './components/Checkbox'
+export { default as CheckboxField } from './components/CheckboxField'
+export { default as Dropdown }     from './components/Dropdown'
+export { default as Footer }       from './components/Footer'
+export { default as InputField }   from './components/InputField'
+export { default as PanelHeader }  from './components/PanelHeader'
+export { default as Pill }         from './components/Pill'
+export { default as Radio }        from './components/Radio'
+export { default as Search }       from './components/Search'
+export { default as SelectField }  from './components/SelectField'
+export { default as Toggle }       from './components/Toggle'
+export { default as PrimaryTabs, { PrimaryTabOption } } from './components/Tabs'
